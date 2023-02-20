a6ef70a2ec4d0cb980662df2fb67fe8931526c16
ocitocit <ocritocrit@gmail.com>
2023-02-20 14:05:13 +0700

    changing firebase project

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+'use client';
+
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { GoPlus } from 'react-icons/go';
+import { db } from '../firebase';
 
 function NewChat() {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const createNewChat = async () => {
+    const doc = await addDoc(collection(db, 'users', session?.user?.email!, 'chats'), {
+      userId: session?.user?.email!,
+      createdAt: serverTimestamp()
+    });
+    console.log(doc)
+    router.push(`/chat/${doc.id}`);
+
+  };
+
   return (
-    <div className="relative mb-1 h-14 w-4/5">
+    <div onClick={createNewChat} className="relative mb-1 h-14 w-4/5">
       <button className="btn btnBig aniButton group justify-center bg-[#10A37F] hover:bg-white ">
         <GoPlus className="h-8 w-8 text-white group-hover:text-[#10a37f]" />
       </button>
--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCiXMUYAwsS5AUH9kOLYunjbwJA0PRnayY',
-  authDomain: 'chat-gpt-clone-7faa7.firebaseapp.com',
-  projectId: 'chat-gpt-clone-7faa7',
-  storageBucket: 'chat-gpt-clone-7faa7.appspot.com',
-  messagingSenderId: '95137140557',
-  appId: '1:95137140557:web:b25af9437129c9c4e8b104'
+  apiKey: "AIzaSyBLbiT4F8pZj4Tos0fMhG3_uDlipZjMdJg",
+  authDomain: "new-clone-chatgpt.firebaseapp.com",
+  projectId: "new-clone-chatgpt",
+  storageBucket: "new-clone-chatgpt.appspot.com",
+  messagingSenderId: "253572357842",
+  appId: "1:253572357842:web:0439b4c6be4c2d01a6970b"
 };
 
 // Initialize Firebase
